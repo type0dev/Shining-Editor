3e2fb3030dc6ce3707cbcd9df71339fd1b3aee51
madhatter <thinktech2go@gmail.com>
2018-08-28 10:26:24 -0400

    uploaded project

---
+++ Editor/Forms/Credits.Designer.vb
@@ -0,0 +1,100 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Credits
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Credits))
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.RichTextBox1 = New System.Windows.Forms.RichTextBox()
+        Me.LinkLabel1 = New System.Windows.Forms.LinkLabel()
+        Me.LinkLabel2 = New System.Windows.Forms.LinkLabel()
+        Me.SuspendLayout()
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Segoe UI", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(16, 9)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(215, 17)
+        Me.Label3.TabIndex = 7
+        Me.Label3.Text = "Created by Type_0_Dev / madhatter"
+        '
+        'RichTextBox1
+        '
+        Me.RichTextBox1.BackColor = System.Drawing.SystemColors.Menu
+        Me.RichTextBox1.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.RichTextBox1.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.RichTextBox1.Font = New System.Drawing.Font("Segoe UI", 9.75!, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.RichTextBox1.Location = New System.Drawing.Point(18, 40)
+        Me.RichTextBox1.Name = "RichTextBox1"
+        Me.RichTextBox1.ReadOnly = True
+        Me.RichTextBox1.Size = New System.Drawing.Size(195, 42)
+        Me.RichTextBox1.TabIndex = 11
+        Me.RichTextBox1.Text = "Special Thanks: velvetmxo" & Global.Microsoft.VisualBasic.ChrW(10) & "The whole Switch community"
+        '
+        'LinkLabel1
+        '
+        Me.LinkLabel1.AutoSize = True
+        Me.LinkLabel1.Font = New System.Drawing.Font("Segoe UI", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.LinkLabel1.Location = New System.Drawing.Point(13, 81)
+        Me.LinkLabel1.Name = "LinkLabel1"
+        Me.LinkLabel1.Size = New System.Drawing.Size(47, 13)
+        Me.LinkLabel1.TabIndex = 12
+        Me.LinkLabel1.TabStop = True
+        Me.LinkLabel1.Text = "Patreon"
+        '
+        'LinkLabel2
+        '
+        Me.LinkLabel2.AutoSize = True
+        Me.LinkLabel2.Font = New System.Drawing.Font("Segoe UI", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.LinkLabel2.Location = New System.Drawing.Point(146, 81)
+        Me.LinkLabel2.Name = "LinkLabel2"
+        Me.LinkLabel2.Size = New System.Drawing.Size(97, 13)
+        Me.LinkLabel2.TabIndex = 13
+        Me.LinkLabel2.TabStop = True
+        Me.LinkLabel2.Text = "Report a problem"
+        '
+        'Credits
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(255, 105)
+        Me.Controls.Add(Me.LinkLabel2)
+        Me.Controls.Add(Me.LinkLabel1)
+        Me.Controls.Add(Me.RichTextBox1)
+        Me.Controls.Add(Me.Label3)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "Credits"
+        Me.Text = "Credits"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label3 As Label
+    Friend WithEvents RichTextBox1 As RichTextBox
+    Friend WithEvents LinkLabel1 As LinkLabel
+    Friend WithEvents LinkLabel2 As LinkLabel
+End Class
---
+++ Editor/Forms/XC2MainForm.Designer.vb
@@ -0,0 +1,181 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class XC2MainForm
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.SaveBtn = New System.Windows.Forms.Button()
+        Me.SaveAsFileBtn = New System.Windows.Forms.Button()
+        Me.OpenFileBtn = New System.Windows.Forms.Button()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.CharacterEditor = New ShiningEditor.XC2FieldTypeEditor()
+        Me.SigilsEditor = New ShiningEditor.XC2FieldTypeEditor()
+        Me.AspectsEditor = New ShiningEditor.XC2FieldTypeEditor()
+        Me.MaterialsEditor = New ShiningEditor.XC2FieldTypeEditor()
+        Me.ConsumablesEditor = New ShiningEditor.XC2FieldTypeEditor()
+        Me.PlayerInfoEditor = New ShiningEditor.XC2FieldTypeEditor()
+        Me.SuspendLayout()
+        '
+        'SaveBtn
+        '
+        Me.SaveBtn.Font = New System.Drawing.Font("Segoe UI", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.SaveBtn.Location = New System.Drawing.Point(521, 365)
+        Me.SaveBtn.Name = "SaveBtn"
+        Me.SaveBtn.Size = New System.Drawing.Size(76, 30)
+        Me.SaveBtn.TabIndex = 15
+        Me.SaveBtn.Text = "Save"
+        Me.SaveBtn.UseVisualStyleBackColor = True
+        '
+        'SaveAsFileBtn
+        '
+        Me.SaveAsFileBtn.Font = New System.Drawing.Font("Segoe UI", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.SaveAsFileBtn.Location = New System.Drawing.Point(603, 365)
+        Me.SaveAsFileBtn.Name = "SaveAsFileBtn"
+        Me.SaveAsFileBtn.Size = New System.Drawing.Size(76, 30)
+        Me.SaveAsFileBtn.TabIndex = 14
+        Me.SaveAsFileBtn.Text = "Save As..."
+        Me.SaveAsFileBtn.UseVisualStyleBackColor = True
+        '
+        'OpenFileBtn
+        '
+        Me.OpenFileBtn.Font = New System.Drawing.Font("Segoe UI", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.OpenFileBtn.Location = New System.Drawing.Point(438, 365)
+        Me.OpenFileBtn.Name = "OpenFileBtn"
+        Me.OpenFileBtn.Size = New System.Drawing.Size(77, 30)
+        Me.OpenFileBtn.TabIndex = 13
+        Me.OpenFileBtn.Text = "Open"
+        Me.OpenFileBtn.UseVisualStyleBackColor = True
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(752, 365)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(89, 30)
+        Me.Button1.TabIndex = 17
+        Me.Button1.Text = "Credits"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'CharacterEditor
+        '
+        Me.CharacterEditor.CanEdit = False
+        Me.CharacterEditor.CurrentValue = Nothing
+        Me.CharacterEditor.DataSource = Nothing
+        Me.CharacterEditor.FieldTypeLabel = Nothing
+        Me.CharacterEditor.FieldValueLabel = "Value"
+        Me.CharacterEditor.Location = New System.Drawing.Point(436, 12)
+        Me.CharacterEditor.Name = "CharacterEditor"
+        Me.CharacterEditor.Size = New System.Drawing.Size(405, 93)
+        Me.CharacterEditor.TabIndex = 22
+        '
+        'SigilsEditor
+        '
+        Me.SigilsEditor.CanEdit = False
+        Me.SigilsEditor.CurrentValue = Nothing
+        Me.SigilsEditor.DataSource = Nothing
+        Me.SigilsEditor.FieldTypeLabel = Nothing
+        Me.SigilsEditor.FieldValueLabel = "Value"
+        Me.SigilsEditor.Location = New System.Drawing.Point(436, 226)
+        Me.SigilsEditor.Name = "SigilsEditor"
+        Me.SigilsEditor.Size = New System.Drawing.Size(405, 93)
+        Me.SigilsEditor.TabIndex = 21
+        '
+        'AspectsEditor
+        '
+        Me.AspectsEditor.CanEdit = False
+        Me.AspectsEditor.CurrentValue = Nothing
+        Me.AspectsEditor.DataSource = Nothing
+        Me.AspectsEditor.FieldTypeLabel = Nothing
+        Me.AspectsEditor.FieldValueLabel = "Value"
+        Me.AspectsEditor.Location = New System.Drawing.Point(436, 111)
+        Me.AspectsEditor.Name = "AspectsEditor"
+        Me.AspectsEditor.Size = New System.Drawing.Size(405, 93)
+        Me.AspectsEditor.TabIndex = 20
+        '
+        'MaterialsEditor
+        '
+        Me.MaterialsEditor.CanEdit = False
+        Me.MaterialsEditor.CurrentValue = Nothing
+        Me.MaterialsEditor.DataSource = Nothing
+        Me.MaterialsEditor.FieldTypeLabel = Nothing
+        Me.MaterialsEditor.FieldValueLabel = "Value"
+        Me.MaterialsEditor.Location = New System.Drawing.Point(13, 226)
+        Me.MaterialsEditor.Name = "MaterialsEditor"
+        Me.MaterialsEditor.Size = New System.Drawing.Size(405, 93)
+        Me.MaterialsEditor.TabIndex = 19
+        '
+        'ConsumablesEditor
+        '
+        Me.ConsumablesEditor.CanEdit = False
+        Me.ConsumablesEditor.CurrentValue = Nothing
+        Me.ConsumablesEditor.DataSource = Nothing
+        Me.ConsumablesEditor.FieldTypeLabel = Nothing
+        Me.ConsumablesEditor.FieldValueLabel = "Value"
+        Me.ConsumablesEditor.Location = New System.Drawing.Point(12, 111)
+        Me.ConsumablesEditor.Name = "ConsumablesEditor"
+        Me.ConsumablesEditor.Size = New System.Drawing.Size(405, 93)
+        Me.ConsumablesEditor.TabIndex = 18
+        '
+        'PlayerInfoEditor
+        '
+        Me.PlayerInfoEditor.CanEdit = False
+        Me.PlayerInfoEditor.CurrentValue = Nothing
+        Me.PlayerInfoEditor.DataSource = Nothing
+        Me.PlayerInfoEditor.FieldTypeLabel = Nothing
+        Me.PlayerInfoEditor.FieldValueLabel = "Value"
+        Me.PlayerInfoEditor.Location = New System.Drawing.Point(12, 12)
+        Me.PlayerInfoEditor.Name = "PlayerInfoEditor"
+        Me.PlayerInfoEditor.Size = New System.Drawing.Size(402, 93)
+        Me.PlayerInfoEditor.TabIndex = 16
+        '
+        'XC2MainForm
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(846, 416)
+        Me.Controls.Add(Me.CharacterEditor)
+        Me.Controls.Add(Me.SigilsEditor)
+        Me.Controls.Add(Me.AspectsEditor)
+        Me.Controls.Add(Me.MaterialsEditor)
+        Me.Controls.Add(Me.ConsumablesEditor)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.PlayerInfoEditor)
+        Me.Controls.Add(Me.SaveBtn)
+        Me.Controls.Add(Me.SaveAsFileBtn)
+        Me.Controls.Add(Me.OpenFileBtn)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D
+        Me.HelpButton = True
+        Me.MaximizeBox = False
+        Me.Name = "XC2MainForm"
+        Me.Text = "Shining Resonance:  Save Editor"
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents PlayerInfoEditor As XC2FieldTypeEditor
+    Friend WithEvents SaveBtn As Button
+    Friend WithEvents SaveAsFileBtn As Button
+    Friend WithEvents OpenFileBtn As Button
+    Friend WithEvents Button1 As Button
+    Friend WithEvents ConsumablesEditor As XC2FieldTypeEditor
+    Friend WithEvents MaterialsEditor As XC2FieldTypeEditor
+    Friend WithEvents AspectsEditor As XC2FieldTypeEditor
+    Friend WithEvents SigilsEditor As XC2FieldTypeEditor
+    Friend WithEvents CharacterEditor As XC2FieldTypeEditor
+End Class
