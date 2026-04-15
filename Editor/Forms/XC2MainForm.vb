603a459789dd0d20ab686d59350d13939d269c10
CriminalMuppet <thinktech2go@gmail.com>
2026-04-15 17:16:06 -0400

    Update XC2MainForm.vb

--- Editor/Forms/XC2MainForm.vb
+++ Editor/Forms/XC2MainForm.vb
@@ -1,4 +1,4 @@
-﻿Imports System.ComponentModel
+Imports System.ComponentModel
 Imports System.IO
 
 Public Class XC2MainForm
@@ -6,27 +6,33 @@ Public Class XC2MainForm
     Implements INotifyPropertyChanged
 
     Private Event PropertyChanged As PropertyChangedEventHandler Implements INotifyPropertyChanged.PropertyChanged
+
     Private Const FILE_DIALOG_FILTER = "Save files ( PLAY00 )|PLAY00|All files (*.*)|*.*"
+
     Private FilePath As String = Nothing
     Private FileBytes As Byte() = Nothing
 
-    ''' <summary>
-    ''' Whether a file is loaded and that file can be edited.
-    ''' </summary>
     Public Property CanEdit As Boolean
         Get
             Return _canEdit
         End Get
         Set(value As Boolean)
             _canEdit = value
-            RaisePropertyChanged(New PropertyChangedEventArgs("CanEdit"))
+            RaisePropertyChanged(New PropertyChangedEventArgs(NameOf(CanEdit)))
         End Set
     End Property
     Private _canEdit As Boolean = False
 
-    ''' <summary>
-    ''' Presents the user with an open file dialog, then loads the file they specify.
-    ''' </summary>
+    Private ReadOnly ReadFromFileBytes As Func(Of HexDataInfo, Integer) =
+        Function(hexData)
+            Return HexEditor.ReadAsDecimal(FileBytes, hexData)
+        End Function
+
+    Private ReadOnly SetFileBytes As Action(Of HexDataInfo, Integer) =
+        Sub(hexData, value)
+            HexEditor.SaveDecimalValue(FileBytes, hexData, value)
+        End Sub
+
     Private Sub OpenFile(btn As Button, e As EventArgs) Handles OpenFileBtn.Click
         Dim openDialog = New OpenFileDialog With {.Filter = FILE_DIALOG_FILTER}
         Dim dialogResult = openDialog.ShowDialog()
@@ -37,15 +43,16 @@ Public Class XC2MainForm
                 FileBytes = File.ReadAllBytes(FilePath)
                 CanEdit = True
             Catch ex As IOException
-                ' Handle exception here...
+                MessageBox.Show("Unable to read the selected save file.", "Open File", MessageBoxButtons.OK, MessageBoxIcon.Error)
             End Try
         End If
     End Sub
 
-    ''' <summary>
-    ''' Saves the currently loaded file, overwriting the currently selected file.
-    ''' </summary>
     Private Sub SaveFile(btn As Button, e As EventArgs) Handles SaveBtn.Click
+        If FileBytes Is Nothing OrElse String.IsNullOrWhiteSpace(FilePath) Then
+            Return
+        End If
+
         Dim dialogResult = MessageBox.Show("Are you sure you want to overwrite the currently loaded file?", "Save File", MessageBoxButtons.YesNo)
 
         If dialogResult = DialogResult.Yes Then
@@ -53,10 +60,11 @@ Public Class XC2MainForm
         End If
     End Sub
 
-    ''' <summary>
-    ''' Presents the user with a save file dialog, then saves the currently loaded copy of the save file to the location they specify.
-    ''' </summary>
     Private Sub SaveFileAs(btn As Button, e As EventArgs) Handles SaveAsFileBtn.Click
+        If FileBytes Is Nothing Then
+            Return
+        End If
+
         Dim saveDialog = New SaveFileDialog With {.Filter = FILE_DIALOG_FILTER}
         Dim dialogResult = saveDialog.ShowDialog()
 
@@ -66,26 +74,8 @@ Public Class XC2MainForm
         End If
     End Sub
 
-    ''' <summary>
-    ''' Read bytes from file data in memory.
-    ''' </summary>
-    Private ReadFromFileBytes As Func(Of HexDataInfo, Integer) = Function(hexData)
-                                                                     Return HexEditor.ReadAsDecimal(FileBytes, hexData)
-                                                                 End Function
-
-    ''' <summary>
-    ''' Save bytes to file data in memory.
-    ''' </summary>
-    Private SetFileBytes As Action(Of HexDataInfo, Integer) = Sub(hexData, value)
-                                                                  HexEditor.SaveDecimalValue(FileBytes, hexData, value)
-                                                              End Sub
-
-    ''' <summary>
-    ''' Handles the initialization of the player info editor control.
-    ''' </summary>
     Private Sub OnPlayerInfoEditorLoaded(sender As XC2FieldTypeEditor, e As EventArgs) Handles PlayerInfoEditor.Load
-
-        PlayerInfoEditor.Configure(XC2Data.PlayerInfoOffsets, "Items", ReadFromFileBytes, SetFileBytes)
+        PlayerInfoEditor.Configure(XC2Data.PlayerInfoOffsets, "Player Info", ReadFromFileBytes, SetFileBytes)
         PlayerInfoEditor.DataBindings.Add(New Binding(NameOf(PlayerInfoEditor.CanEdit), Me, NameOf(CanEdit), False, DataSourceUpdateMode.OnPropertyChanged))
 
         ConsumablesEditor.Configure(XC2Data.ConsumablesOffsets, "Consumables", ReadFromFileBytes, SetFileBytes)
@@ -102,45 +92,22 @@ Public Class XC2MainForm
 
         CharacterEditor.Configure(XC2Data.CharacterOffsets, "Characters", ReadFromFileBytes, SetFileBytes)
         CharacterEditor.DataBindings.Add(New Binding(NameOf(CharacterEditor.CanEdit), Me, NameOf(CanEdit), False, DataSourceUpdateMode.OnPropertyChanged))
-
-    End Sub
-
-    ''' <summary>
-    ''' Tell the UI to update after a value was programatically changed. If this is not called, the UI will not know a value has updated.
-    ''' </summary>
-    Private Sub RaisePropertyChanged(ByVal e As PropertyChangedEventArgs)
-        RaiseEvent PropertyChanged(Me, e)
     End Sub
 
     Private Sub XC2MainForm_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-
         PlayerInfoEditor.BackColor = Color.Transparent
         CharacterEditor.BackColor = Color.Transparent
         ConsumablesEditor.BackColor = Color.Transparent
         AspectsEditor.BackColor = Color.Transparent
         MaterialsEditor.BackColor = Color.Transparent
         SigilsEditor.BackColor = Color.Transparent
-
-    End Sub
-
-    Private Sub OnPlayerInfoEditorLoaded(sender As Object, e As EventArgs) Handles PlayerInfoEditor.Load
-
-    End Sub
-
-    Private Sub BladeEditor_Load(sender As Object, e As EventArgs)
-
-    End Sub
-
-    Private Sub XC2FieldTypeEditor1_Load(sender As Object, e As EventArgs)
-
     End Sub
 
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
         Credits.Visible = True
-
     End Sub
 
-    Private Sub ConsumablesEditor_Load(sender As Object, e As EventArgs) Handles ConsumablesEditor.Load
-
+    Private Sub RaisePropertyChanged(ByVal e As PropertyChangedEventArgs)
+        RaiseEvent PropertyChanged(Me, e)
     End Sub
 End Class
